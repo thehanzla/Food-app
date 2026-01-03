49fad59be14bd5ff5fe07d64ca0d2bad0446c1ad
Hanzla Tariq <thehanzalatariq@gmail.com>
2026-01-03 13:18:38 +0500

    Update frontend to point thereto new working backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
-const API_BASE_URL = 'https://food-2tsasxcaf-thehanzlas-projects.vercel.app/api'
+const API_BASE_URL = 'https://food-five-rho-61.vercel.app/api'
 
 
 const api = axios.create({

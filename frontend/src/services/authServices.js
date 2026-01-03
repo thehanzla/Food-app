27d5c75fa8af3031f694a0c3b953a7c0eefc1ba0
Hanzla Tariq <thehanzalatariq@gmail.com>
2026-01-03 13:00:33 +0500

    Update from user request

--- frontend/src/services/authServices.js
+++ frontend/src/services/authServices.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
-const API_BASE_URL = 'https://food-neupfrzzz-thehanzlas-projects.vercel.app/api';
+const API_BASE_URL = 'https://food-25kpu44ws-thehanzlas-projects.vercel.app/api';
 
 
 // Create axios instance
